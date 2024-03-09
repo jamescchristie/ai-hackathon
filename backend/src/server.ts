b38b601167708c694b194af000c48402f9d05f6f
James Christie <jameschristie@Jamess-MacBook-Pro.local>
2024-03-09 16:10:52 +0000

    wip

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,10 @@ import express, { Express, Request, Response } from "express";
 import multer from "multer";
 import App, { UserInputs } from "./application/index";
 
+const ROUTES = {
+  IMAGES: "images",
+};
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/");
@@ -16,30 +20,27 @@ const upload = multer({ dest: "uploads/", storage });
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const ROUTES = {
-  IMAGES: "images",
-};
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
 // respond with "hello world" when a GET request is made to the homepage
 app.post(`/${ROUTES.IMAGES}`, upload.single("image"), async (req, res) => {
   const userPrefs = req.body as UserInputs;
   const filePath = req.file?.path;
+  console.log(filePath);
   if (!filePath) {
     throw new Error("No file path found");
   }
 
   const analysis = await App.analyseImage(filePath);
-  // await App.getStyleSuggestions(analysis);
-  // const csv = await App.analyseCsv();
-  // await App.pickStylesFromCSV(csv);
+  // // await App.getStyleSuggestions(analysis);
+  // // const csv = await App.analyseCsv();
+  // // await App.pickStylesFromCSV(csv);
   const pickedStyles = await App.curateLook(analysis, userPrefs);
   res.send(pickedStyles);
 });
